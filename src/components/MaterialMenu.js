bfdc363100289d6134a458d5d8a9251f3403656b
Eli <pleanbean@gmail.com>
2020-02-21 16:19:44 +0100

    add links to menu

--- src/components/MaterialMenu.js
+++ src/components/MaterialMenu.js
@@ -1,71 +1,75 @@
-import React from 'react'
-import { makeStyles } from '@material-ui/core/styles'
-import styled from '@emotion/styled'
-import { css } from '@emotion/core'
-import AppBar from '@material-ui/core/AppBar'
-import Toolbar from '@material-ui/core/Toolbar'
-import Typography from '@material-ui/core/Typography'
-import Button from '@material-ui/core/Button'
-import IconButton from '@material-ui/core/IconButton'
-import MenuIcon from '@material-ui/icons/Menu'
-import Hidden from '@material-ui/core/Hidden'
-import Drawer from '@material-ui/core/Drawer'
-import List from '@material-ui/core/List'
-import ListItem from '@material-ui/core/ListItem'
-import ListItemText from '@material-ui/core/ListItemText'
-import withWidth from '@material-ui/core/withWidth'
-import HeaderImagePath from '../../static/images/sfdc-header.png'
-
-const HeaderImage = styled.img`
-  max-width: 100px;
-  filter: invert(1);
-`
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import styled from "@emotion/styled";
+import { css } from "@emotion/core";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
+import IconButton from "@material-ui/core/IconButton";
+import MenuIcon from "@material-ui/icons/Menu";
+import Hidden from "@material-ui/core/Hidden";
+import Drawer from "@material-ui/core/Drawer";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemText from "@material-ui/core/ListItemText";
+import withWidth from "@material-ui/core/withWidth";
+import HeaderImagePath from "../../static/images/sfdc-header.png";
+import { Link } from "gatsby";
 
 const useStyles = makeStyles(theme => ({
   root: {
-    flexGrow: 1,
+    flexGrow: 1
   },
   menuButton: {
     marginRight: theme.spacing(1),
     fontWeight: 100,
-    color: '#fafafa',
+    color: "#fafafa"
   },
   title: {
     flexGrow: 1,
     fontWeight: 100,
-    fontSize: '1.2em',
-    minHeight: '64px',
-    marginTop: '10px',
+    fontSize: "1.2em",
+    minHeight: "64px",
+    marginTop: "10px"
   },
   list: {
-    width: 250,
+    width: 250
   },
   listItem: {
-    textAlign: 'center',
+    textAlign: "center"
   },
-}))
+  headerImage: {
+    maxWidth: "100px",
+    filter: "invert(1)"
+  },
+  link: {
+    textDecoration: "none",
+    color: "inherit"
+  }
+}));
 
 const ButtonAppBar = props => {
-  const { width, onMenuClick } = props
-  const classes = useStyles()
+  const { width, onMenuClick } = props;
+  const classes = useStyles();
 
   const [state, setState] = React.useState({
     top: false,
     left: false,
     bottom: false,
-    right: false,
-  })
+    right: false
+  });
 
   const toggleDrawer = (side, open) => event => {
     if (
-      event.type === 'keydown' &&
-      (event.key === 'Tab' || event.key === 'Shift')
+      event.type === "keydown" &&
+      (event.key === "Tab" || event.key === "Shift")
     ) {
-      return
+      return;
     }
 
-    setState({ ...state, [side]: open })
-  }
+    setState({ ...state, [side]: open });
+  };
 
   const sideList = side => (
     <div
@@ -76,14 +80,14 @@ const ButtonAppBar = props => {
     >
       <List>
         {[
-          'Home',
-          'Calendar',
-          'Donate',
-          'About Us',
-          'Teachers',
-          'Newsletter',
-          'Volunteer',
-          'Resources',
+          "Home",
+          "Calendar",
+          "Donate",
+          "About Us",
+          "Teachers",
+          "Newsletter",
+          "Volunteer",
+          "Resources"
         ].map((text, index) => (
           <ListItem className={classes.listItem} button key={text}>
             <ListItemText primary={text} />
@@ -91,37 +95,73 @@ const ButtonAppBar = props => {
         ))}
       </List>
     </div>
-  )
+  );
 
   return (
     <div className={classes.root}>
       <AppBar
         css={css`
-          background-color: '#fafafa';
+          background-color: "#fafafa";
         `}
         elevation={0}
         position="fixed"
       >
         <Toolbar>
           <Typography className={classes.title}>
-            <HeaderImage src={HeaderImagePath} alt="Header Image" />
+            <img
+              className={classes.headerImage}
+              src={HeaderImagePath}
+              alt="Header Image"
+            />
           </Typography>
           <Hidden smDown>
-            <Button className={classes.menuButton}>Home</Button>
-            <Button className={classes.menuButton}>Calendar</Button>
-            <Button className={classes.menuButton}>Donate</Button>
-            <Button className={classes.menuButton}>About Us</Button>
-            <Button className={classes.menuButton}>Teachers</Button>
-            <Button className={classes.menuButton}>Newsletter</Button>
-            <Button className={classes.menuButton}>Volunteer</Button>
-            <Button className={classes.menuButton}>Resources</Button>
+            <Button className={classes.menuButton}>
+              <Link to="/" className={classes.link}>
+                Home
+              </Link>
+            </Button>
+            <Button className={classes.menuButton}>
+              <Link to="/calendar" className={classes.link}>
+                Calendar
+              </Link>
+            </Button>
+            <Button className={classes.menuButton}>
+              <Link to="/donate" className={classes.link}>
+                Donate
+              </Link>
+            </Button>
+            <Button className={classes.menuButton}>
+              <Link to="/about-us" className={classes.link}>
+                About Us
+              </Link>
+            </Button>
+            <Button className={classes.menuButton}>
+              <Link to="/teachers" className={classes.link}>
+                Teachers
+              </Link>
+            </Button>
+            <Button className={classes.menuButton}>
+              <Link to="/newsletter" className={classes.link}>
+                Newsletter
+              </Link>
+            </Button>
+            <Button className={classes.menuButton}>
+              <Link to="/volunteer" className={classes.link}>
+                Volunteer
+              </Link>
+            </Button>
+            <Button className={classes.menuButton}>
+              <Link to="/resources" className={classes.link}>
+                Resources
+              </Link>
+            </Button>
           </Hidden>
           <Hidden mdUp>
             <IconButton
               edge="start"
               color="inherit"
               aria-label="menu"
-              onClick={toggleDrawer('right', true)}
+              onClick={toggleDrawer("right", true)}
             >
               <MenuIcon />
             </IconButton>
@@ -130,16 +170,16 @@ const ButtonAppBar = props => {
             <Drawer
               anchor="right"
               open={state.right}
-              onClose={toggleDrawer('right', false)}
+              onClose={toggleDrawer("right", false)}
             >
-              {sideList('right')}
+              {sideList("right")}
             </Drawer>
           </Hidden>
         </Toolbar>
       </AppBar>
       <Toolbar />
     </div>
-  )
-}
+  );
+};
 
-export default withWidth()(ButtonAppBar)
+export default withWidth()(ButtonAppBar);
