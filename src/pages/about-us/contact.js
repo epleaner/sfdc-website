6b63601e94082d2297130bda84f05b8af9add2af
Eli <pleanbean@gmail.com>
2020-02-22 13:17:51 +0100

    Contact page put together

--- src/pages/about-us/contact.js
+++ src/pages/about-us/contact.js
@@ -2,6 +2,7 @@ import React from "react";
 import Layout from "../../components/Layout";
 import SEO from "../../components/SEO";
 import Grid from "@material-ui/core/Grid";
+import Typography from "@material-ui/core/Typography";
 
 const Contact = () => {
   return (
@@ -10,7 +11,99 @@ const Contact = () => {
         title="Contact"
         description="San Francisco Dharma Collective Contact Page"
       />
-      <Grid container />
+      <Grid container spacing={6}>
+        <Grid item xs={12}>
+          <Typography align="center" variant="h2">
+            SF Dharma Collective Committees Contact Information
+          </Typography>
+        </Grid>
+        <Grid item xs={12} alignItems="center" container>
+          <Grid item xs={12} alignItems="center" container>
+            <Grid item xs={12} sm={2}>
+              <Typography variant="h6">Programming</Typography>
+            </Grid>
+            <Grid item xs={12} sm={10}>
+              <Typography variant="body1">
+                <a href="mailto:programming.sfdharmacollective@gmail.com">
+                  programming.sfdharmacollective@gmail.com
+                </a>
+              </Typography>
+            </Grid>
+          </Grid>
+          <Grid item xs={12} alignItems="center" container>
+            <Grid item xs={12} sm={2}>
+              <Typography variant="h6">Volunteers</Typography>
+            </Grid>
+            <Grid item xs={12} sm={10}>
+              <Typography variant="body1">
+                <a href="mailto:sfdcvolunteers@gmail.com">
+                  sfdcvolunteers@gmail.com
+                </a>
+              </Typography>
+            </Grid>
+          </Grid>
+          <Grid item xs={12} alignItems="center" container>
+            <Grid item xs={12} sm={2}>
+              <Typography variant="h6">Marketing</Typography>
+            </Grid>
+            <Grid item xs={12} sm={10}>
+              <Typography variant="body1">
+                <a href="mailto:sfdharmacollective@gmail.com">
+                  sfdharmacollective@gmail.com
+                </a>
+              </Typography>
+            </Grid>
+          </Grid>
+          <Grid item xs={12} alignItems="center" container>
+            <Grid item xs={12} sm={2}>
+              <Typography variant="h6">Finance</Typography>
+            </Grid>
+            <Grid item xs={12} sm={10}>
+              <Typography variant="body1">
+                <a href="mailto:finance.sfdharmacollective@gmail.com ">
+                  finance.sfdharmacollective@gmail.com
+                </a>
+              </Typography>
+            </Grid>
+          </Grid>
+          <Grid item xs={12} alignItems="center" container>
+            <Grid item xs={12} sm={2}>
+              <Typography variant="h6">Buildings & Grounds</Typography>
+            </Grid>
+            <Grid item xs={12} sm={10}>
+              <Typography variant="body1">
+                <a href="mailto:sfdharmacollective@gmail.com">
+                  sfdharmacollective@gmail.com
+                </a>
+              </Typography>
+            </Grid>
+          </Grid>
+          <Grid item xs={12} alignItems="center" container>
+            <Grid item xs={12} sm={2}>
+              <Typography variant="h6">Teacher Outreach</Typography>
+            </Grid>
+            <Grid item xs={12} sm={10}>
+              <Typography variant="body1">
+                <a href="mailto:sfdharmacollective@gmail.com">
+                  sfdharmacollective@gmail.com
+                </a>
+              </Typography>
+            </Grid>
+          </Grid>
+          <Grid item xs={12} alignItems="center" container>
+            <Grid item xs={12} sm={2}>
+              <Typography variant="h6">Arts & Community</Typography>
+            </Grid>
+            <Grid item xs={12} sm={10}>
+              <Typography variant="body1">
+                <a href="mailto:art.sfdharmacollective@gmail.com">
+                  art.sfdharmacollective@gmail.com
+                </a>
+              </Typography>
+            </Grid>
+          </Grid>
+        </Grid>
+      </Grid>
     </Layout>
   );
 };
