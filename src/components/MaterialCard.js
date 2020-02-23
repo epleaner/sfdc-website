9fc9ccf4a6f26d4daaf37190ddeced2bde4a6ab1
Eli <pleanbean@gmail.com>
2020-02-23 11:22:01 +0100

    Add a card component for teachers

---
+++ src/components/MaterialCard.js
@@ -0,0 +1,37 @@
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Card from "@material-ui/core/Card";
+import CardContent from "@material-ui/core/CardContent";
+import CardMedia from "@material-ui/core/CardMedia";
+import Button from "@material-ui/core/Button";
+import Typography from "@material-ui/core/Typography";
+
+const useStyles = makeStyles({
+  root: {
+    maxWidth: 345
+  },
+  media: {
+    height: 140
+  }
+});
+
+const MaterialCard = props => {
+  const { imgPath, name, url } = props;
+  const classes = useStyles();
+
+  return (
+    <Card className={classes.root}>
+      <CardMedia className={classes.media} image={imgPath} title={name} />
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="h2">
+          {name}
+        </Typography>
+        <Typography variant="body2" color="textSecondary" component="p">
+          {url}
+        </Typography>
+      </CardContent>
+    </Card>
+  );
+};
+
+export default MaterialCard;
