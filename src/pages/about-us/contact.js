b55f3df4a5e2602d19774ca7547b53b855d8912c
Eli <pleanbean@gmail.com>
2020-02-24 10:07:26 +0100

    Make contact data be more DRY

--- src/pages/about-us/contact.js
+++ src/pages/about-us/contact.js
@@ -3,6 +3,17 @@ import Layout from "../../components/Layout";
 import SEO from "../../components/SEO";
 import Grid from "@material-ui/core/Grid";
 import Typography from "@material-ui/core/Typography";
+import Box from "@material-ui/core/Box";
+
+const contactData = [
+  { name: "Programming", email: "programming.sfdharmacollective@gmail.com" },
+  { name: "Volunteers", email: "sfdcvolunteers@gmail.com" },
+  { name: "Marketing", email: "sfdharmacollective@gmail.com" },
+  { name: "Finance", email: "finance.sfdharmacollective@gmail.com " },
+  { name: "Buildings", email: "sfdharmacollective@gmail.com" },
+  { name: "Teacher", email: "sfdharmacollective@gmail.com" },
+  { name: "Arts", email: "art.sfdharmacollective@gmail.com" }
+];
 
 const Contact = () => {
   return (
@@ -11,97 +22,27 @@ const Contact = () => {
         title="Contact"
         description="San Francisco Dharma Collective Contact Page"
       />
-      <Grid container spacing={6}>
+      <Grid container>
         <Grid item xs={12}>
-          <Typography align="center" variant="h2">
-            SF Dharma Collective Committees Contact Information
-          </Typography>
+          <Box mb={5}>
+            <Typography align="center" variant="h2">
+              SF Dharma Collective Committees Contact Information
+            </Typography>
+          </Box>
         </Grid>
         <Grid item xs={12} alignItems="center" container>
-          <Grid item xs={12} alignItems="center" container>
-            <Grid item xs={12} sm={2}>
-              <Typography variant="h6">Programming</Typography>
+          {contactData.map((data, index) => (
+            <Grid item key={index} xs={12} alignItems="center" container>
+              <Grid item xs={12} sm={2}>
+                <Typography variant="h6">{data.name}</Typography>
+              </Grid>
+              <Grid item xs={12} sm={10}>
+                <Typography variant="body1">
+                  <a href={`mailto:${data.email}`}>{data.email}</a>
+                </Typography>
+              </Grid>
             </Grid>
-            <Grid item xs={12} sm={10}>
-              <Typography variant="body1">
-                <a href="mailto:programming.sfdharmacollective@gmail.com">
-                  programming.sfdharmacollective@gmail.com
-                </a>
-              </Typography>
-            </Grid>
-          </Grid>
-          <Grid item xs={12} alignItems="center" container>
-            <Grid item xs={12} sm={2}>
-              <Typography variant="h6">Volunteers</Typography>
-            </Grid>
-            <Grid item xs={12} sm={10}>
-              <Typography variant="body1">
-                <a href="mailto:sfdcvolunteers@gmail.com">
-                  sfdcvolunteers@gmail.com
-                </a>
-              </Typography>
-            </Grid>
-          </Grid>
-          <Grid item xs={12} alignItems="center" container>
-            <Grid item xs={12} sm={2}>
-              <Typography variant="h6">Marketing</Typography>
-            </Grid>
-            <Grid item xs={12} sm={10}>
-              <Typography variant="body1">
-                <a href="mailto:sfdharmacollective@gmail.com">
-                  sfdharmacollective@gmail.com
-                </a>
-              </Typography>
-            </Grid>
-          </Grid>
-          <Grid item xs={12} alignItems="center" container>
-            <Grid item xs={12} sm={2}>
-              <Typography variant="h6">Finance</Typography>
-            </Grid>
-            <Grid item xs={12} sm={10}>
-              <Typography variant="body1">
-                <a href="mailto:finance.sfdharmacollective@gmail.com ">
-                  finance.sfdharmacollective@gmail.com
-                </a>
-              </Typography>
-            </Grid>
-          </Grid>
-          <Grid item xs={12} alignItems="center" container>
-            <Grid item xs={12} sm={2}>
-              <Typography variant="h6">Buildings & Grounds</Typography>
-            </Grid>
-            <Grid item xs={12} sm={10}>
-              <Typography variant="body1">
-                <a href="mailto:sfdharmacollective@gmail.com">
-                  sfdharmacollective@gmail.com
-                </a>
-              </Typography>
-            </Grid>
-          </Grid>
-          <Grid item xs={12} alignItems="center" container>
-            <Grid item xs={12} sm={2}>
-              <Typography variant="h6">Teacher Outreach</Typography>
-            </Grid>
-            <Grid item xs={12} sm={10}>
-              <Typography variant="body1">
-                <a href="mailto:sfdharmacollective@gmail.com">
-                  sfdharmacollective@gmail.com
-                </a>
-              </Typography>
-            </Grid>
-          </Grid>
-          <Grid item xs={12} alignItems="center" container>
-            <Grid item xs={12} sm={2}>
-              <Typography variant="h6">Arts & Community</Typography>
-            </Grid>
-            <Grid item xs={12} sm={10}>
-              <Typography variant="body1">
-                <a href="mailto:art.sfdharmacollective@gmail.com">
-                  art.sfdharmacollective@gmail.com
-                </a>
-              </Typography>
-            </Grid>
-          </Grid>
+          ))}
         </Grid>
       </Grid>
     </Layout>
