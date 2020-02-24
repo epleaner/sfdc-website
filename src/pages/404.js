c5cae6dc89a45e99e097214cf627af33728955f6
Eli <pleanbean@gmail.com>
2020-02-24 12:01:29 +0100

    Update 404 page without emotion

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,25 +1,34 @@
 import React from "react";
-import { Link } from "gatsby";
-import styled from "@emotion/styled";
 import Layout from "../components/Layout";
+import Grid from "@material-ui/core/Grid";
+import Typography from "@material-ui/core/Typography";
+import Box from "@material-ui/core/Box";
 import SEO from "../components/SEO";
 
-const Text = styled.p`
-  text-align: center;
-  line-height: 1.6;
-  a {
-    color: ${props => props.theme.colors.text};
-  }
-`;
-
-const NotFoundPage = () => (
-  <Layout>
-    <SEO title="404" description="Page Not Found" />
-    <Text>
-      Please return <Link to="/">home</Link> or use the menu to navigate to a
-      different page.
-    </Text>
-  </Layout>
-);
+const NotFoundPage = () => {
+  return (
+    <Layout>
+      <SEO
+        title="Not Found"
+        description="San Francisco Dharma Collective Not Found Page"
+      />
+      <Grid container>
+        <Grid item xs={12}>
+          <Box mb={3}>
+            <Typography align="center" variant="h2">
+              Form is emptiness, emptiness is form. Form is not other than
+              emptiness; emptiness is not other than form.
+            </Typography>
+          </Box>
+        </Grid>
+        <Grid item xs={12}>
+          <Typography align="center" variant="body1">
+            Sorry, the page you're looking for cannot be found.
+          </Typography>
+        </Grid>
+      </Grid>
+    </Layout>
+  );
+};
 
 export default NotFoundPage;
