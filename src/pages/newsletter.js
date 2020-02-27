ad24c0497c8f08a823312e6346bc3302552a8ff9
Eli <pleanbean@gmail.com>
2020-02-27 15:07:35 +0100

    Reformatting using eslint

--- src/pages/newsletter.js
+++ src/pages/newsletter.js
@@ -1,26 +1,31 @@
-import React, {useEffect} from "react";
-import Layout from "../components/Layout";
-import SEO from "../components/SEO";
-import Grid from "@material-ui/core/Grid";
-import Typography from "@material-ui/core/Typography";
+import React, {useEffect} from 'react';
+import Layout from '../components/Layout';
+import SEO from '../components/SEO';
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
 
 const Newsletter = () => {
   useEffect(() => {
-    fetch("/.netlify/functions/google-calendar")
-      .then(response => response.json())
-      .then(console.log)
+    fetch('/.netlify/functions/google-calendar')
+        .then((response) => response.json())
+        .then(console.log);
   }, []);
 
-  return (<Layout>
-    <SEO title="Newsletter" description="San Francisco Dharma Collective Newsletter Page"/>
-    <Grid container>
-      <Grid item xs={12}>
-        <Typography align="center" variant="h2">
-          Coming soon...
-        </Typography>
+  return (
+    <Layout>
+      <SEO
+        title="Newsletter"
+        description="San Francisco Dharma Collective Newsletter Page"
+      />
+      <Grid container>
+        <Grid item xs={12}>
+          <Typography align="center" variant="h2">
+            Coming soon...
+          </Typography>
+        </Grid>
       </Grid>
-    </Grid>
-  </Layout>);
+    </Layout>
+  );
 };
 
 export default Newsletter;
