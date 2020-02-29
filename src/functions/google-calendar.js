f8f703bb7788d7ca8488e97a0673de042729b668
Eli <pleanbean@gmail.com>
2020-02-29 17:07:51 +0100

    Linting

--- src/functions/google-calendar.js
+++ src/functions/google-calendar.js
@@ -1,54 +1,57 @@
-const { google } = require("googleapis");
-let privatekey = require("../../credentials.json")
+const moment = require('moment');
+const {google} = require('googleapis');
 
-const calendarId = "6lmk34aeh3mpas0kop9ve8hc94@group.calendar.google.com";
+const privatekey = require('../../credentials.json');
+
+const calendarId = '6lmk34aeh3mpas0kop9ve8hc94@group.calendar.google.com';
 
 function handleError(error, callback) {
-  console.log("There was an error...", error);
+  console.log('There was an error...', error);
   callback(null, {
     statusCode: 400,
     body: JSON.stringify(error),
-  })
+  });
 }
 
 export function handler(event, context, callback) {
   const jwtClient = new google.auth.JWT(
-    privatekey.client_email,
-    null,
-    privatekey.private_key,
-    ["https://www.googleapis.com/auth/calendar"]
-  )
+      privatekey.client_email,
+      null,
+      privatekey.private_key,
+      ['https://www.googleapis.com/auth/calendar'],
+  );
 
   jwtClient.authorize(function(error, tokens) {
     if (error) {
-      handleError(error, callback)
+      handleError(error, callback);
     } else {
-      console.log("Successfully connected to Google API");
+      console.log('Successfully connected to Google API');
     }
-  })
+  });
 
-  const calendar = google.calendar("v3")
+  const calendar = google.calendar('v3');
 
-  console.log("Listing calendar events for ", calendarId)
+  console.log('Listing calendar events for ', calendarId);
 
   calendar.events.list(
-    {
-      auth: jwtClient,
-      calendarId: calendarId
-    },
-    function(error, response) {
-      if (error) {
-        handleError(error, callback)
-      }
+      {
+        auth: jwtClient,
+        calendarId: calendarId,
+        timeMin: moment()
+            .startOf('week')
+            .format(),
+      },
+      function(error, response) {
+        if (error) {
+          handleError(error, callback);
+        } else {
+          console.log('Success!');
 
-      else {
-        console.log("Success!")
-
-        callback(null, {
-          statusCode: 200,
-          body: JSON.stringify(response),
-        })
-      }
-    }
-  )
+          callback(null, {
+            statusCode: 200,
+            body: JSON.stringify(response),
+          });
+        }
+      },
+  );
 }
