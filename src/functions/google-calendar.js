3da278dc11839b3da7271d373f9fdad43260f9b4
Eli <pleanbean@gmail.com>
2020-03-03 11:38:45 +0100

    Add phony credentials file so creds work in prod and dev environments

--- src/functions/google-calendar.js
+++ src/functions/google-calendar.js
@@ -1,7 +1,7 @@
 const moment = require('moment');
 const {google} = require('googleapis');
 
-// const privatekey = require('../../credentials.json');
+const devCredentials = require('../../credentials.json');
 
 const calendarId = '6lmk34aeh3mpas0kop9ve8hc94@group.calendar.google.com';
 
@@ -14,46 +14,46 @@ function handleError(error, callback) {
 }
 
 export function handler(event, context, callback) {
-  const jwtClient = new google.auth.JWT(
-      process.env.GCAL_CLIENT_EMAIL,
-      // privatekey.client_email,
-      null,
-      process.env.GCAL_PRIVATE_KEY,
-      // privatekey.private_key,
-      ['https://www.googleapis.com/auth/calendar'],
-  );
+  const clientEmail =
+    process.env.GATSBY_GCAL_CLIENT_EMAIL || devCredentials.client_email;
+  const privateKey =
+    process.env.GATSBY_GCAL_PRIVATE_KEY || devCredentials.private_key;
+
+  const jwtClient = new google.auth.JWT(clientEmail, null, privateKey, [
+    'https://www.googleapis.com/auth/calendar',
+  ]);
 
   jwtClient.authorize(function(error, tokens) {
     if (error) {
       handleError(error, callback);
     } else {
       console.log('Successfully connected to Google API');
+
+      const calendar = google.calendar('v3');
+
+      console.log('Listing calendar events for ', calendarId);
+
+      calendar.events.list(
+          {
+            auth: jwtClient,
+            calendarId: calendarId,
+            timeMin: moment()
+                .startOf('week')
+                .format(),
+          },
+          function(error, response) {
+            if (error) {
+              handleError(error, callback);
+            } else {
+              console.log('Success!');
+
+              callback(null, {
+                statusCode: 200,
+                body: JSON.stringify(response),
+              });
+            }
+          },
+      );
     }
   });
-
-  const calendar = google.calendar('v3');
-
-  console.log('Listing calendar events for ', calendarId);
-
-  calendar.events.list(
-      {
-        auth: jwtClient,
-        calendarId: calendarId,
-        timeMin: moment()
-            .startOf('week')
-            .format(),
-      },
-      function(error, response) {
-        if (error) {
-          handleError(error, callback);
-        } else {
-          console.log('Success!');
-
-          callback(null, {
-            statusCode: 200,
-            body: JSON.stringify(response),
-          });
-        }
-      },
-  );
 }
