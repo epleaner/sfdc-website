6daf0d5858667c479aed2e1adbb7d8031ad98494
Eli <pleanbean@gmail.com>
2020-03-03 15:41:22 +0100

    Add light blue to palette

--- src/styles/muiTheme.js
+++ src/styles/muiTheme.js
@@ -1,39 +1,40 @@
-import { createMuiTheme, responsiveFontSizes } from "@material-ui/core/styles";
+import {createMuiTheme, responsiveFontSizes} from '@material-ui/core/styles';
 
 const MuiTheme = responsiveFontSizes(
-  createMuiTheme({
-    palette: {
-      backgroundColor: "#fafafa"
-    },
-    typography: {
-      fontFamily:
+    createMuiTheme({
+      palette: {
+        backgroundColor: '#fafafa',
+        lightBlue: '#5ac8cd',
+      },
+      typography: {
+        fontFamily:
         '-apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif',
-      h1: {
-        fontWeight: 100,
-        fontSize: "5.5rem"
+        h1: {
+          fontWeight: 100,
+          fontSize: '5.5rem',
+        },
+        h2: {
+          fontSize: '2.5rem',
+          fontWeight: 100,
+        },
+        h3: {
+          fontSize: '2rem',
+          fontWeight: 100,
+        },
+        h4: {
+          fontSize: '1.5rem',
+          fontWeight: 100,
+        },
+        h5: {
+          fontSize: '1.25rem',
+          fontWeight: 100,
+        },
+        h6: {
+          fontSize: '1.15rem',
+          fontWeight: 100,
+        },
       },
-      h2: {
-        fontSize: "2.5rem",
-        fontWeight: 100
-      },
-      h3: {
-        fontSize: "2rem",
-        fontWeight: 100
-      },
-      h4: {
-        fontSize: "1.5rem",
-        fontWeight: 100
-      },
-      h5: {
-        fontSize: "1.25rem",
-        fontWeight: 100
-      },
-      h6: {
-        fontSize: "1.15rem",
-        fontWeight: 100
-      }
-    }
-  })
+    }),
 );
 
 export default MuiTheme;
