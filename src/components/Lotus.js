da940fccd26468e51dea9f9b61e6dc8e0288df06
Eli <pleanbean@gmail.com>
2020-03-06 17:16:32 +0100

    WIP lotus made from css

---
+++ src/components/Lotus.js
@@ -0,0 +1,70 @@
+// import React from 'react';
+// import {makeStyles} from '@material-ui/core/styles';
+//
+// const useStyles = makeStyles({
+//   lotus: {
+//     width: '100px',
+//     height: '100px',
+//     backgroundColor: 'rgba(150, 220, 222, 0.85)',
+//     borderRadius: '100% 0%',
+//     position: 'absolute',
+//   },
+//   lotus1: {
+//     transform: 'rotate(55deg)',
+//     top: 170px;
+//
+// left: -43px;
+//   },
+//   lotus2: {
+//     transform: 'rotate(75deg)',
+//     top: 148px;
+//
+// left: -37px;
+//   },
+//   lotus3: {
+//     transform: 'rotate(95deg)',
+//     top: 130px;
+//
+// left: -21px;
+//   },
+//   lotus4: {
+//     transform: 'rotate(135deg)',
+//
+//   },
+//   lotus5: {
+//     transform: 'rotate(175deg)',
+//     top: 129px;
+//
+// left: 67px;
+//
+//   },
+//   lotus6: {
+//     transform: 'rotate(195deg)',
+//     top: 150px;
+//
+// left: 85px;
+//   },
+//   lotus7: {
+//     transform: 'rotate(215deg)',
+//     top: 170px;
+//
+// left: 90px;
+//   },
+// });
+//
+// const Lotus = () => {
+//   const classes = useStyles();
+//   return (
+//     <div>
+//       <div className={[classes.lotus, classes.lotus1].join(' ')} />
+//       <div className={[classes.lotus, classes.lotus2].join(' ')} />
+//       <div className={[classes.lotus, classes.lotus3].join(' ')} />
+//       <div className={[classes.lotus, classes.lotus4].join(' ')} />
+//       <div className={[classes.lotus, classes.lotus5].join(' ')} />
+//       <div className={[classes.lotus, classes.lotus6].join(' ')} />
+//       <div className={[classes.lotus, classes.lotus7].join(' ')} />
+//     </div>
+//   );
+// };
+//
+// export default Lotus;
