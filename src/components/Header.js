ff7815b7422b0856f163a7046b9a94e1355a56ca
Eli <pleanbean@gmail.com>
2020-03-08 16:24:44 +0100

    Flatter header HTML layout

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,24 +1,9 @@
 import React from 'react';
 import {Location} from '@reach/router';
-import {makeStyles} from '@material-ui/core/styles';
 import MaterialMenu from './MaterialMenu';
 
-const useStyles = makeStyles({
-  header: {
-    background: '#fafafa',
-  },
-});
-
-const Header = (props) => {
-  const classes = useStyles();
-
-  return (
-    <div className={classes.header}>
-      <Location>
-        {(locationProps) => <MaterialMenu {...locationProps} />}
-      </Location>
-    </div>
-  );
-};
+const Header = () => (
+  <Location>{(locationProps) => <MaterialMenu {...locationProps} />}</Location>
+);
 
 export default Header;
