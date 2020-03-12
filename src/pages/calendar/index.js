4c919a9e39d53082ad4b918be9bf17cc36a38715
Eli <pleanbean@gmail.com>
2020-03-12 16:01:38 +0100

    Style calendar to make more minimal

--- src/pages/calendar/index.js
+++ src/pages/calendar/index.js
@@ -64,7 +64,7 @@ const Calendar = () => {
           <Box className={classes.iframe} mt={3}>
             <iframe
               title="sfdc-calendar-iframe"
-              src="https://calendar.google.com/calendar/embed?mode=AGENDA&amp;height=600&amp;wkst=1&amp;bgcolor=%23FFFFFF&amp;src=6lmk34aeh3mpas0kop9ve8hc94%40group.calendar.google.com&amp;color=%2342104A&amp;ctz=America%2FLos_Angeles"
+              src="https://calendar.google.com/calendar/embed?mode=AGENDA&amp;height=600&amp;wkst=1&amp;bgcolor=%23FFFFFF&amp;src=6lmk34aeh3mpas0kop9ve8hc94%40group.calendar.google.com&amp;showTitle=0;color=%2342104A&amp;ctz=America%2FLos_Angeles"
               width="800"
               height="600"
               frameBorder="0"
