a6cf713c4fd3e0d1a19eb1c8c900650240adb1c9
Eli <eli.i.pleaner@gmail.com>
2020-04-11 15:23:34 +0200

    Add google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,4 +1,4 @@
-require('dotenv').config({
+require("dotenv").config({
   path: `.env.${process.env.NODE_ENV}`,
 });
 
@@ -8,11 +8,11 @@ const contentfulConfig = {
   host: process.env.CONTENTFUL_HOST,
 };
 
-const {spaceId, accessToken} = contentfulConfig;
+const { spaceId, accessToken } = contentfulConfig;
 
 if (!spaceId || !accessToken) {
   throw new Error(
-      'Contentful spaceId and the access token need to be provided.',
+    "Contentful spaceId and the access token need to be provided."
   );
 }
 
@@ -21,22 +21,22 @@ module.exports = {
   // read more: https://www.gatsbyjs.org/docs/api-proxy/#advanced-proxying
   developMiddleware: (app) => {
     app.use(
-        '/.netlify/functions/',
-        require('http-proxy-middleware').createProxyMiddleware({
-          target: 'http://localhost:9000',
-          pathRewrite: {
-            '/.netlify/functions/': '',
-          },
-        }),
+      "/.netlify/functions/",
+      require("http-proxy-middleware").createProxyMiddleware({
+        target: "http://localhost:9000",
+        pathRewrite: {
+          "/.netlify/functions/": "",
+        },
+      })
     );
   },
   siteMetadata: {
-    title: 'San Francisco Dharma Collective',
+    title: "San Francisco Dharma Collective",
     description:
-      'San Francisco Dharma Collective\'s website: The SF Dharma Collective is a community-led sangha. Come meditate with us!',
-    siteUrl: 'https://sfdharmacollective.org',
-    image: '/src/assets/images/sfdc-header.png',
-    basePath: '/',
+      "San Francisco Dharma Collective's website: The SF Dharma Collective is a community-led sangha. Come meditate with us!",
+    siteUrl: "https://sfdharmacollective.org",
+    image: "/src/assets/images/sfdc-header.png",
+    basePath: "/",
   },
   plugins: [
     `gatsby-transformer-sharp`,
@@ -47,8 +47,8 @@ module.exports = {
         path: `${__dirname}/src/assets`,
       },
     },
-    'gatsby-plugin-material-ui',
-    'gatsby-plugin-react-helmet',
+    "gatsby-plugin-material-ui",
+    "gatsby-plugin-react-helmet",
     {
       resolve: `gatsby-transformer-remark`,
       options: {
@@ -62,26 +62,26 @@ module.exports = {
     },
     `gatsby-plugin-catch-links`,
     {
-      resolve: 'gatsby-plugin-google-analytics',
+      resolve: "gatsby-plugin-gtag",
       options: {
         trackingId: process.env.GOOGLE_ANALYTICS,
         head: true,
       },
     },
-    'gatsby-plugin-sitemap',
+    "gatsby-plugin-sitemap",
     {
-      resolve: 'gatsby-plugin-manifest',
+      resolve: "gatsby-plugin-manifest",
       options: {
-        name: 'SF Dharma Collective',
-        short_name: 'SFDC',
-        start_url: '/',
-        background_color: '#ffffff',
-        theme_color: '#ffffff',
-        display: 'minimal-ui',
-        icon: './src/assets/images/favicon.png',
+        name: "SF Dharma Collective",
+        short_name: "SFDC",
+        start_url: "/",
+        background_color: "#ffffff",
+        theme_color: "#ffffff",
+        display: "minimal-ui",
+        icon: "./src/assets/images/favicon.png",
       },
     },
-    'gatsby-plugin-offline',
+    "gatsby-plugin-offline",
     {
       resolve: `gatsby-plugin-schema-snapshot`,
       options: {
@@ -89,20 +89,20 @@ module.exports = {
         update: process.env.GATSBY_UPDATE_SCHEMA_SNAPSHOT,
       },
     },
-    'gatsby-plugin-netlify',
+    "gatsby-plugin-netlify",
     {
-      resolve: 'gatsby-plugin-mailchimp',
+      resolve: "gatsby-plugin-mailchimp",
       options: {
         endpoint: process.env.GATSBY_MAILCHIMP_SUBSCRIBE_ENDPOINT,
       },
     },
     {
-      resolve: 'gatsby-source-contentful',
+      resolve: "gatsby-source-contentful",
       options: contentfulConfig,
     },
     {
       resolve: `gatsby-plugin-create-client-paths`,
-      options: {prefixes: [`/events/*`]},
+      options: { prefixes: [`/events/*`] },
     },
   ],
 };
