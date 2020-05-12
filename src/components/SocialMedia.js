ee19673f05ff5727dc1f2e2c35fc6d7e96b58522
Eli <eli.i.pleaner@gmail.com>
2020-05-12 16:34:08 +0200

    Change twitter handle

--- src/components/SocialMedia.js
+++ src/components/SocialMedia.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {makeStyles} from '@material-ui/core/styles';
+import { makeStyles } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
 import Box from '@material-ui/core/Box';
 import Link from '@material-ui/core/Link';
@@ -10,8 +10,8 @@ import YouTubeIcon from '@material-ui/icons/YouTube';
 
 const useStyles = makeStyles((theme) => ({
   icon: {
-    'transition': 'all .25s ease-in-out',
-    'color': theme.palette.primaryBlue4,
+    transition: 'all .25s ease-in-out',
+    color: theme.palette.primaryBlue4,
     '&:hover': {
       color: theme.palette.primaryBlue0,
     },
@@ -22,42 +22,42 @@ const SocialMedia = () => {
   const classes = useStyles();
 
   return (
-    <Grid container justify="center">
-      <Grid item container justify="center" xs={2}>
+    <Grid container justify='center'>
+      <Grid item container justify='center' xs={2}>
         <Link
-          target="_blank"
-          rel="noopener noreferrer"
-          href="https://www.facebook.com/sfdharmacollective"
+          target='_blank'
+          rel='noopener noreferrer'
+          href='https://www.facebook.com/sfdharmacollective'
           className={classes.icon}
         >
           <FacebookIcon />
         </Link>
       </Grid>
-      <Grid item container justify="center" xs={2}>
+      <Grid item container justify='center' xs={2}>
         <Link
-          target="_blank"
-          rel="noopener noreferrer"
-          href="https://twitter.com/SFDC_Folsom"
+          target='_blank'
+          rel='noopener noreferrer'
+          href='https://twitter.com/DharmaClctive'
           className={classes.icon}
         >
           <TwitterIcon />
         </Link>
       </Grid>
-      <Grid item container justify="center" xs={2}>
+      <Grid item container justify='center' xs={2}>
         <Link
-          target="_blank"
-          rel="noopener noreferrer"
-          href="https://www.instagram.com/sfdharmacollective/"
+          target='_blank'
+          rel='noopener noreferrer'
+          href='https://www.instagram.com/sfdharmacollective/'
           className={classes.icon}
         >
           <InstagramIcon />
         </Link>
       </Grid>
-      <Grid item container justify="center" xs={2}>
+      <Grid item container justify='center' xs={2}>
         <Link
-          target="_blank"
-          rel="noopener noreferrer"
-          href="https://www.youtube.com/channel/UCxrOXx3uqvfbYhs0A7twjSw/featured"
+          target='_blank'
+          rel='noopener noreferrer'
+          href='https://www.youtube.com/channel/UCxrOXx3uqvfbYhs0A7twjSw/featured'
           className={classes.icon}
         >
           <YouTubeIcon />
