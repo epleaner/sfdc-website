fd3535e82aa034f4bd9dd0502f61bd2178b334d9
Eli <eli.i.pleaner@gmail.com>
2020-05-12 16:34:16 +0200

    Linting

--- src/pages/resources/other-offerings.js
+++ src/pages/resources/other-offerings.js
@@ -7,32 +7,32 @@ import React from 'react';
 import SEO from '../../components/SEO';
 import Typography from '@material-ui/core/Typography';
 import ZenHospiceImagePath from '../../assets/images/zen-hospice.jpg';
-import {makeStyles} from '@material-ui/core/styles';
+import { makeStyles } from '@material-ui/core/styles';
 
 const useStyles = makeStyles({
   description: {
     '& a': {
-      'color': 'rgba(62,149,153,1)',
-      'textDecoration': 'none',
+      color: 'rgba(62,149,153,1)',
+      textDecoration: 'none',
       '&:hover': {
         textDecoration: 'underline',
       },
     },
   },
-  avatar: {width: '200px', height: '200px'},
+  avatar: { width: '200px', height: '200px' },
 });
 
 const offeringsData = [
   {
     name: 'Zen Hospice Trainings',
     body:
-      'Zen Hospice holds periodic trainings in our space. For more information, visit the <a href=\'https://www.zenhospice.org/education-training/mce/\'>Zen Hospice Project</a>.',
+      "Zen Hospice holds periodic trainings in our space. For more information, visit the <a href='https://www.zenhospice.org/education-training/mce/'>Zen Hospice Project</a>.",
     imgSrc: ZenHospiceImagePath,
   },
   {
     name: 'Consciousness Hacking',
     body:
-      '<a href=\'https://www.facebook.com/consciousnesshackingbay\'>Consciousness Hacking SF Bay Area</a> holds periodic events in our space.',
+      "<a href='https://www.facebook.com/consciousnesshackingbay'>Consciousness Hacking SF Bay Area</a> holds periodic events in our space.",
     imgSrc: ConsciousHackingImagePath,
   },
 ];
@@ -42,25 +42,25 @@ const Offerings = () => {
   return (
     <Layout>
       <SEO
-        title="Other Offerings"
-        description="San Francisco Dharma Collective Other Offerings Page"
+        title='Other Offerings'
+        description='San Francisco Dharma Collective Other Offerings Page'
       />
       <Grid container>
         <Grid item xs={12}>
           <Box mb={4}>
-            <Typography align="center" variant="h2" component="h1">
-              Other Offerings at 2701 Folsom
+            <Typography align='center' variant='h2' component='h1'>
+              Other Offerings at SFDC
             </Typography>
           </Box>
         </Grid>
         {offeringsData.map((offering, index) => (
           <Box mb={8}>
-            <Grid item xs={12} container key={index} component="article">
+            <Grid item xs={12} container key={index} component='article'>
               <Grid item container xs={12} sm={9}>
-                <Grid item container alignContent="flex-end" xs={12}>
+                <Grid item container alignContent='flex-end' xs={12}>
                   <Grid item xs={12}>
                     <Box mb={2}>
-                      <Typography variant="h4" component="h1">
+                      <Typography variant='h4' component='h1'>
                         {offering.name}
                       </Typography>
                     </Box>
@@ -70,18 +70,18 @@ const Offerings = () => {
                   <Box mb={2}>
                     <Typography
                       className={classes.description}
-                      variant="body1"
+                      variant='body1'
                       dangerouslySetInnerHTML={{
                         __html: `${offering.body.replace(
-                            /<a/g,
-                            `<a target='_blank' rel='noopener noreferrer'`,
+                          /<a/g,
+                          `<a target='_blank' rel='noopener noreferrer'`
                         )}`,
                       }}
                     />
                   </Box>
                 </Grid>
               </Grid>
-              <Grid item container justify="center" xs={12} sm={3}>
+              <Grid item container justify='center' xs={12} sm={3}>
                 <Avatar
                   className={classes.avatar}
                   src={offering.imgSrc}
