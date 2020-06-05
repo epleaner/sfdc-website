b4f7424a71956448b7b3b68bc88870138b881db9
Eli <eli.i.pleaner@gmail.com>
2020-06-05 15:02:15 +0200

    Add solidarity banner to home page

--- src/graphql-fragments/ContentfulPageFragment.js
+++ src/graphql-fragments/ContentfulPageFragment.js
@@ -7,5 +7,10 @@ export const ContentfulPageFragment = graphql`
     contentSections {
       ...ContentfulContentSectionFragment
     }
+    popUp {
+      content {
+        json
+      }
+    }
   }
 `;
