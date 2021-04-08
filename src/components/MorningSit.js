61f32b30858c85fd99a1598b38d7b8450fc54a63
Eli <eliipleaner@gmail.com>
2021-04-08 16:25:24 -0700

    Add PST

--- src/components/MorningSit.js
+++ src/components/MorningSit.js
@@ -1,18 +1,18 @@
-import Box from "@material-ui/core/Box";
-import Divider from "@material-ui/core/Divider";
-import Grid from "@material-ui/core/Grid";
-import Link from "@material-ui/core/Link";
-import { makeStyles } from "@material-ui/core/styles";
-import Typography from "@material-ui/core/Typography";
-import { graphql, useStaticQuery } from "gatsby";
-import Img from "gatsby-image";
-import React from "react";
+import Box from '@material-ui/core/Box';
+import Divider from '@material-ui/core/Divider';
+import Grid from '@material-ui/core/Grid';
+import Link from '@material-ui/core/Link';
+import { makeStyles } from '@material-ui/core/styles';
+import Typography from '@material-ui/core/Typography';
+import { graphql, useStaticQuery } from 'gatsby';
+import Img from 'gatsby-image';
+import React from 'react';
 
 const useStyles = makeStyles((theme) => ({
-  centerAligned: { alignSelf: "center" },
+  centerAligned: { alignSelf: 'center' },
   anchor: {
-    color: "rgba(62,149,153,1)",
-    textDecoration: "none",
+    color: 'rgba(62,149,153,1)',
+    textDecoration: 'none',
   },
 }));
 
@@ -34,59 +34,58 @@ export default () => {
     <>
       <Grid item xs={12} sm={3} className={classes.centerAligned}>
         <Box mb={3}>
-          <Typography variant="h3" align="center" component="h1">
+          <Typography variant='h3' align='center' component='h1'>
             Morning sits
           </Typography>
         </Box>
       </Grid>
       <Grid item xs={12} sm={6} container>
-        <Grid item xs={12} container alignContent="center" justify="center">
+        <Grid item xs={12} container alignContent='center' justify='center'>
           <Box mb={1}>
-            <Typography variant="h4" align="center" component="h2">
+            <Typography variant='h4' align='center' component='h2'>
               Every day
             </Typography>
           </Box>
         </Grid>
-        <Grid item xs={12} container alignContent="center" justify="center">
+        <Grid item xs={12} container alignContent='center' justify='center'>
           <Box>
-            <Typography variant="h6" align="center" component="h3">
-              7:30 - 8:15am
+            <Typography variant='h6' align='center' component='h3'>
+              7:30 - 8:15am PST
             </Typography>
           </Box>
         </Grid>
-        <Grid item xs={12} container alignContent="center" justify="center">
+        <Grid item xs={12} container alignContent='center' justify='center'>
           <Box my={3} mx={2}>
-            <Typography variant="body1" align="center" component="p">
-              Join us online:{" "}
+            <Typography variant='body1' align='center' component='p'>
+              Join us online:{' '}
               <Link
-                target="_blank"
-                rel="noopener noreferrer"
+                target='_blank'
+                rel='noopener noreferrer'
                 className={classes.anchor}
-                href="//bit.ly/sfdharma"
-              >
+                href='//bit.ly/sfdharma'>
                 bit.ly/sfdharma
-              </Link>{" "}
+              </Link>{' '}
               (password: <b>108108</b>)
             </Typography>
           </Box>
         </Grid>
-        <Grid item xs={12} container alignContent="center" justify="center">
+        <Grid item xs={12} container alignContent='center' justify='center'>
           <Box mb={3} mx={2}>
-            <Typography variant="body2" align="center" component="p">
+            <Typography variant='body2' align='center' component='p'>
               Arriving late and leaving early is okay for morning sits!
             </Typography>
           </Box>
         </Grid>
       </Grid>
       <Grid item xs={12} sm={3} container>
-        <Grid item xs={12} container justify="center">
+        <Grid item xs={12} container justify='center'>
           <Box mt={3}>
-            <Img fixed={data.file.childImageSharp.fixed} alt="Lotus" />
+            <Img fixed={data.file.childImageSharp.fixed} alt='Lotus' />
           </Box>
         </Grid>
         <Grid item xs={12}>
           <Box mb={2}>
-            <Typography variant="caption" align="center" component="aside">
+            <Typography variant='caption' align='center' component='aside'>
               Morning sits are peer-led.
             </Typography>
           </Box>
