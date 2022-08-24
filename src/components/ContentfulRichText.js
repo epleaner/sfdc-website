a91108fdca12823d66d17743c44c12a9999f9892
Eli Pleaner <eliipleaner@gmail.com>
2022-08-23 18:06:44 -0700

    remove log

--- src/components/ContentfulRichText.js
+++ src/components/ContentfulRichText.js
@@ -9,8 +9,6 @@ import Typography from '@material-ui/core/Typography';
 const ContentfulRichText = (props) => {
   const { json } = props;
 
-  console.log(json);
-
   const renderingOptions = {
     renderMark: {},
     renderNode: {
