21996742d9fd08b2bdf784a8142576c7ec9c8cb9
Eli Pleaner <eliipleaner@gmail.com>
2022-08-26 13:43:24 -0700

    Refactor donate to share one component

--- src/pages/donate.js
+++ src/pages/donate.js
@@ -1,15 +1,11 @@
 import React from 'react';
 import { graphql, useStaticQuery } from 'gatsby';
 
-import Box from '@material-ui/core/Box';
 import Grid from '@material-ui/core/Grid';
-import Typography from '@material-ui/core/Typography';
 
-import DonateButton from '../components/DonateButton';
+import DonateComponent from '../components/HomePage/Donate';
 import Layout from '../components/Layout';
 import SEO from '../components/SEO';
-import Venmo from '../components/Venmo';
-import ContentfulRichText from '../components/ContentfulRichText';
 
 const Donate = () => {
   const { pageData } = useStaticQuery(graphql`
@@ -29,37 +25,7 @@ const Donate = () => {
       <Layout>
         <Grid container>
           <Grid item xs={12}>
-            <Box mb={3}>
-              <Typography align='center' variant='h2' component='h1'>
-                {pageData.title}
-              </Typography>
-            </Box>
-          </Grid>
-          <Grid item xs={12}>
-            <Box mb={10}>
-              <Typography align='center' variant='h5' component='h2'>
-                <i>{pageData.subTitle}</i>
-              </Typography>
-            </Box>
-          </Grid>
-          <Grid container item>
-            <Grid item xs={12} container justify='center'>
-              <Box mb={4}>
-                <DonateButton inputText='Teacher, class, general note, et cetera...' />
-              </Box>
-            </Grid>
-            <Grid item xs={12} container justify='center'>
-              <Box mb={6}>
-                <Venmo />
-              </Box>
-            </Grid>
-            {pageData.contentSections.map(({ title, content }) => (
-              <Grid key={title} item xs={12}>
-                <Typography align='center' variant='body1' component='span'>
-                  <ContentfulRichText json={content.json} />
-                </Typography>
-              </Grid>
-            ))}
+            <DonateComponent />
           </Grid>
         </Grid>
       </Layout>
