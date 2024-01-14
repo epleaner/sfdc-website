52d9bf1b2893aee15d0bc3c29e24596dc4f69473
Eli Pleaner <eliipleaner@gmail.com>
2024-01-13 17:26:30 -0800

    fix event images

--- src/components/EventBody/index.js
+++ src/components/EventBody/index.js
@@ -3,14 +3,14 @@ import Box from '@material-ui/core/Box';
 import Grid from '@material-ui/core/Grid';
 import React from 'react';
 import Typography from '@material-ui/core/Typography';
-import {makeStyles} from '@material-ui/core/styles';
+import { makeStyles } from '@material-ui/core/styles';
 
 const useStyles = makeStyles((theme) => ({
   eventDescription: {
-    'marginTop': theme.spacing(2),
+    marginTop: theme.spacing(2),
     '& a': {
-      'color': 'rgba(62,149,153,1)',
-      'textDecoration': 'none',
+      color: 'rgba(62,149,153,1)',
+      textDecoration: 'none',
       '&:hover': {
         textDecoration: 'underline',
         cursor: 'auto',
@@ -34,13 +34,13 @@ const useStyles = makeStyles((theme) => ({
 }));
 
 const EventBody = (props) => {
-  const {attachments, description} = props;
+  const { attachments, description } = props;
 
   const classes = useStyles();
 
   const descriptionWithModifiedAnchors = description.replace(
-      /<a/g,
-      `<a target='_blank' rel='noopener noreferrer'`,
+    /<a/g,
+    `<a target='_blank' rel='noopener noreferrer'`
   );
 
   return (
@@ -50,11 +50,10 @@ const EventBody = (props) => {
         xs={12}
         sm={attachments ? 6 : 12}
         md={attachments ? 8 : 12}
-        className={classes.eventDescriptionContainer}
-      >
+        className={classes.eventDescriptionContainer}>
         <Box mb={4}>
           <Typography
-            variant="body1"
+            variant='body1'
             className={classes.eventDescription}
             dangerouslySetInnerHTML={{
               __html: `${descriptionWithModifiedAnchors || ''}`,
@@ -69,19 +68,20 @@ const EventBody = (props) => {
           xs={12}
           sm={6}
           md={4}
-          className={classes.avatarContainer}
-        >
-          {attachments.map(({fileId, title}) => (
-            <Grid key={fileId} item container justify="center" xs={12}>
-              <Box my={2}>
-                <Avatar
-                  className={classes.avatar}
-                  alt={title}
-                  src={`http://drive.google.com/uc?export=view&id=${fileId}`}
-                />
-              </Box>
-            </Grid>
-          ))}
+          className={classes.avatarContainer}>
+          {attachments.map(({ fileId, title }) => {
+            return (
+              <Grid key={fileId} item container justify='center' xs={12}>
+                <Box my={2}>
+                  <Avatar
+                    className={classes.avatar}
+                    alt={title}
+                    src={`https://lh3.googleusercontent.com/d/${fileId}?authuser=0`}
+                  />
+                </Box>
+              </Grid>
+            );
+          })}
         </Grid>
       )}
     </Grid>
